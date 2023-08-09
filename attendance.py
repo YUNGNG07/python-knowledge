47cfef764443738842a8f658cd25097ef4d2f6ca
Yung Jia Ng <yung-jia.ng@keysight.com>
2023-08-09 09:51:05 +0800

    Version 0.0.1 of attendance.py

--- attendance.py
+++ attendance.py
@@ -1,16 +1,34 @@
 from openpyxl import load_workbook
 import pandas as pd
+import numpy as np
+import datetime
 
 filename = 'Attendance System.xlsx'
+now = datetime.datetime.now().strftime('%d-%b-%Y %H:%M:%S')
 
-# name = str(input('Enter name: '))
-# print('Name is: ' + name)
+def read(name):
+    # Read all Excel data from multiple sheets into one DataFrame
+    sheets = pd.concat(pd.read_excel(filename, sheet_name=None), ignore_index=True)
 
-workbook = load_workbook(filename=filename)
-sheet_num = len(workbook.sheetnames)
+    # Check through every name
+    for names in sheets['Name']:
+        # print(names)
+        sheets = sheets.replace(np.nan, '')
+        if name == names:
+            print('Found ' + name)
+            # Select rows from a DataFrame based on column values
+            details = sheets.loc[sheets['Name'] == name]
+            sheets.at[int(details.index.values), 'Attendance'] = 'Yes'
+            sheets.at[int(details.index.values), 'Timestamp'] = now
+            sheets.to_excel('Output.xlsx')
+        else:
+            continue
+            # print('This person is not in this department')
 
-sheet1 = pd.read_excel(filename, sheet_name=None, index_col=0)
-sheets = pd.concat(sheet1.values())
-print(sheets)
+    print(sheets)
 
-
+if __name__ == '__main__':
+    while True:
+        name = str(input('Enter name: '))
+        # name = 'J'
+        read(name)
